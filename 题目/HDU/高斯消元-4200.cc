d39c22abd22bd20b3d96d2f8616afdab4d9d09aa
seoi2017 <iaswork@foxmail.com>
2017-09-04 07:22:31 +0800

    update

--- 题目/HDU/高斯消元-4200.cc
+++ 题目/HDU/高斯消元-4200.cc
@@ -1,25 +1,27 @@
-//POJ-3185[Accepted]
+//HDU-4200[Accepted]
 //Tag:[高斯消元（线性代数）]
 //Code By HYS.
-//0ms,0.7MB(704KB),1610B
+//15ms,1.7MB(1740KB),1902B
 #include<iostream>
 #include<cstdio>
 #include<cstdlib>
 #include<cmath>
 #include<cstring>
 #include<algorithm>
+#define MX 128
 #define INF 0x3f3f3f3f
-#define MX 32
 using namespace std;
-int n=20,mt[MX][MX],freeyuan[MX],x[MX];
+int mt[MX][MX],T,n,d,x[MX];
 inline int Gauss(){
-    int i=1,t=0;
-    for(int j=1;i<=n,j<=n;i++,j++){
+    int i=1,t=0,r=n;
+    for(int j=1;i<=n,j<=r;i++,j++){
         int index=i;
         for(int k=i+1;k<=n;k++)
             if(mt[k][j]>mt[index][j])index=k;
         if(!mt[index][j]){
-            freeyuan[++t]=j,i--;
+            for(int k=1;k<=n;k++)//把自由元列移到最后
+                swap(mt[k][j],mt[k][r]);
+            t++,j--,i--,r--;
             continue;
         }
         if(index!=i)
@@ -30,16 +32,15 @@ inline int Gauss(){
                 for(int l=1;l<=n+1;l++)
                     mt[k][l]^=mt[i][l];
     }
-    //必须注明的一点：结束消元后，i指针指向的是第一个自由元（全零）方程
-    for(int k=i;k<=n;k++)
+    for(int k=i;k<=n;k++)//别忘了无解情况！
         if(mt[k][n+1])return -1;
-    int res=INF;
-    int s=(1<<t);
+    int res=INF,s=(1<<t);
     for(int k=0;k<s;k++){
         int cnt=0;
+        memset(x,0,sizeof(x));
         for(int j=1;j<=t;j++)
-            if(x[freeyuan[j]]=(bool)(k&(1<<(j-1))))cnt++;
-        for(int j=i-1;j>=1;j--){//所以第i-1个方程才是最后一个非自由元（全零）方程
+            if(x[i+j-1]=(bool)(k&(1<<(j-1))))cnt++;
+        for(int j=i-1;j>=1;j--){
             int tmp=mt[j][n+1];
             for(int l=j+1;l<=n;l++)
                 tmp^=(mt[j][l]&x[l]);
@@ -54,12 +55,19 @@ int main(){
     freopen("io/in","r",stdin);
     freopen("io/out","w",stdout);
 #endif
-    for(int i=1;i<=n;i++)
-        scanf("%d",&mt[i][n+1]),mt[i][i]=1;
-    for(int i=2;i<n;i++)
-        mt[i-1][i]=mt[i+1][i]=1;
-    mt[2][1]=mt[n-1][n]=1;
-    printf("%d\n",Gauss());
+    scanf("%d",&T);
+    while(T--){
+        memset(mt,0,sizeof(mt));
+        scanf("%d%d",&n,&d);
+        for(int i=1;i<=n;i++)
+            scanf("%d",&mt[i][n+1]);
+        for(int i=1;i<=n;i++)
+            for(int j=max(i-d,1);j<=min(i+d,n);j++)
+                mt[j][i]=1;
+        int ans=Gauss();
+        if(ans!=-1)printf("%d\n",ans);
+        else printf("impossible\n");
+    }
 #ifdef LOCAL
     fclose(stdin);
     fclose(stdout);
